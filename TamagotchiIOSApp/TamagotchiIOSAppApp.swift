bc9bbd66c7b726117fdec5eb33de513c6a93a293
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2022-01-20 11:50:07 +0000

    Initial Commit

---
+++ TamagotchiIOSApp/TamagotchiIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiIOSAppApp.swift
+//  TamagotchiIOSApp
+//
+//  Created by Edmunds, Maximilian (NA) on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
